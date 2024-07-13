72e58ab09930f4b3e141fb7c87b44818b3e97db1
firodina <rowdahassan49@gmail.com>
2024-07-13 19:34:24 +0300

    header

---
+++ Amazone-e-commerce/src/components/Header/Header.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { BsSearch } from "react-icons/bs";
+import { SlLocationPin } from "react-icons/sl";
+import { BiCart } from "react-icons/bi";
+import classes from "./header.module.css";
+import LowerHeader from "./LowerHeader";
+
+function Header() {
+  return (
+    <>
+      <section>
+        <div className={classes.header_container}>
+          <div className={classes.logo_contaniner}>
+            {/* {logo} */}
+            <a href="/">
+              <img
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt=""
+              />
+            </a>
+          </div>
+          {/* {delivery} */}
+          <div className={classes.delivery_contanier}>
+            <span>
+              <SlLocationPin />
+            </span>
+            <div>
+              <p>Delivered to</p>
+              <span>Ethiopia</span>
+            </div>
+          </div>
+          {/* {search} */}
+          <div className={classes.search_contanier}>
+            <select name="" id="">
+              <option value="">All</option>
+            </select>
+            <input type="text" name="" id="" placeholder="Search Product" />
+            <BsSearch />
+          </div>
+
+          {/* {rigth side link} */}
+          <div className={classes.order_contanier}>
+            <a href="" className={classes.language}>
+              <img src="https://img.freepik.com/free-vector/illustration-usa-flag_53876-18165.jpg" />
+              <select name="" id="">
+                <option value="">EN</option>
+              </select>
+            </a>
+            <a href="">
+              <p>Sign In</p>
+              <span>Account&List</span>
+            </a>
+            {/* {orders} */}
+            <a href="">
+              <p>returns</p>
+              <span>&Orders</span>
+            </a>
+            {/* {cart} */}
+            <a href="" className={classes.cart}>
+              <BiCart size={35} />
+              <span>0</span>
+            </a>
+          </div>
+        </div>
+      </section>
+      <LowerHeader />
+    </>
+  );
+}
+
+export default Header;
