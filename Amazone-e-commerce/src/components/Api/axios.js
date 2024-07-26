12a8a6e44ac8fdba3c03faa8f1b322d53c19a1a8
firodina <rowdahassan49@gmail.com>
2024-07-26 10:51:07 +0300

    api

--- Amazone-e-commerce/src/components/Api/axios.js
+++ Amazone-e-commerce/src/components/Api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-59dc5/us-central1/api"
+  baseURL: "https://amazon-api-deploy-9l75.onrender.com"
 });
 export {axiosInstance}
